80e6aa31f9f4f474f06403af03f1710c8a9e334f
surajkunwarkcgi <kunwarsuraj2019@gmail.com>
2021-10-15 10:00:52 +0900

    Car sections added

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,18 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 
-function Section() {
+function Section({
+  title,
+  description,
+  backgroundImg,
+  leftBtnText,
+  rightBtnText,
+}) {
   return (
-    <Wrap>
+    <Wrap bgImg={backgroundImg}>
       <ItemText>
-        <h1>Model S</h1>
-        <p>Order Online for Touchless Delivery</p>
+        <h1>{title}</h1>
+        <p>{description}</p>
       </ItemText>
 
-      <ButtonGroup>
-        <LeftButton>Custom Order</LeftButton>
-        <RightButton>Existing Inventory</RightButton>
-      </ButtonGroup>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+          <RightButton>{rightBtnText}</RightButton>
+        </ButtonGroup>
+
+        <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
     </Wrap>
   );
 }
@@ -22,7 +32,7 @@ export default Section;
 const Wrap = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: url("/images/model-s.jpg");
+  background-image: ${(props) => `url("/images/${props.bgImg}")`};
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
@@ -40,6 +50,9 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
   display: flex;
   margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 const LeftButton = styled.div`
@@ -55,6 +68,18 @@ const LeftButton = styled.div`
   text-transform: uppercase;
   font-size: 12px;
   cursor: pointer;
+  margin: 8px;
 `;
 
-const RightButton = styled(LeftButton)``;
+const RightButton = styled(LeftButton)`
+  background-color: #fff;
+  opacity: 0.65;
+  color: #000;
+`;
+
+const DownArrow = styled.img`
+  margin-top: 20px;
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+`;
+const Buttons = styled.div``;
